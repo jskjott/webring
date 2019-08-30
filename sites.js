22e85bc0b730077003ac09d3d280e8268f2d5aba
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-08-30 10:34:39 -0400

    Add vaibhavsagar to sites.js

--- sites.js
+++ sites.js
@@ -8,6 +8,7 @@ const sites = [
 	{ url: 'https://www.fekadu.com', title: 'mfekadu', author: 'Michael Fekadu' },
 	{ url: 'http://www.charstiles.com', title: 'charstiles', author: 'Char Stiles' },
 	{ url: 'https://blog.wesleyac.com', title: 'wesleyac', author: 'Wesley Aptekar-Cassels' },
+	{ url: 'https://vaibhavsagar.com', title: 'vaibhavsagar', author: 'Vaibhav Sagar' }
 	
 ]
 
