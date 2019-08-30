58b16a948737b94e0c13a9251c0b387187926bad
Prayag Chatha <pgchatha@gmail.com>
2019-08-30 14:13:31 -0400

    Update sites.js

--- sites.js
+++ sites.js
@@ -8,8 +8,8 @@ const sites = [
 	{ url: 'https://www.fekadu.com', title: 'mfekadu', author: 'Michael Fekadu' },
 	{ url: 'http://www.charstiles.com', title: 'charstiles', author: 'Char Stiles' },
 	{ url: 'https://blog.wesleyac.com', title: 'wesleyac', author: 'Wesley Aptekar-Cassels' },
-	{ url: 'https://mclare.blog', title: 'mclare', author: 'Maryanne Wachter'},	
-	{ url: 'https://vaibhavsagar.com', title: 'vaibhavsagar', author: 'Vaibhav Sagar' }
+	{ url: 'https://chatha-sphere.github.io/', title: 'pchatha', author: 'Prayag (Nikku) Chatha' }
+	
 ]
 
 export default sites
